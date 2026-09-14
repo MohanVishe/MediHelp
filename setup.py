1f508d46782e2012b5c879b3be315f5e1d0935f8
Mohan Prakash Vishe <114080820+MohanVishe@users.noreply.github.com>
2026-09-14 23:50:08 +0530

    Correct package name and contact email

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = 'Medical Chatbot',
+    name = 'medical-rag-chatbot',
     version= '0.0.0',
     author= 'Mohan Prakash Vishe',
-    author_email= 'mohanvishe1999@gmail.com',
+    author_email= 'mohanvishe.plus@gmail.com',
     packages= find_packages(),
     install_requires = []
 
